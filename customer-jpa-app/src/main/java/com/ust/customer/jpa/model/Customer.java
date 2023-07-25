d0f46554c71ed546b483a529536d97f0fff6fc54
anson1003 <136587011+anson1003@users.noreply.github.com>
2023-07-25 10:13:35 +0530

    Add files via upload

---
+++ customer-jpa-app/src/main/java/com/ust/customer/jpa/model/Customer.java
@@ -0,0 +1,133 @@
+package com.ust.customer.jpa.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+@Entity
+@Table(name="customer_data")
+public class Customer {
+	@Id
+	private int id;
+	
+	@Column(name="customer_name",length=50,nullable=false)
+	private String customerName;
+	
+	@Column(unique=true)
+	private String email;
+	
+	
+	private LocalDate dob;
+	
+	@Enumerated(EnumType.STRING)
+	private UserType type;
+
+	public Customer() {
+	}
+
+	public Customer(int id, String customerName, String email, LocalDate dob, UserType type) {
+		this.id = id;
+		this.customerName = customerName;
+		this.email = email;
+		this.dob = dob;
+		this.type = type;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public UserType getType() {
+		return type;
+	}
+
+	public void setType(UserType type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customerName == null) ? 0 : customerName.hashCode());
+		result = prime * result + ((dob == null) ? 0 : dob.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (customerName == null) {
+			if (other.customerName != null)
+				return false;
+		} else if (!customerName.equals(other.customerName))
+			return false;
+		if (dob == null) {
+			if (other.dob != null)
+				return false;
+		} else if (!dob.equals(other.dob))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+	
+	
+
+}
